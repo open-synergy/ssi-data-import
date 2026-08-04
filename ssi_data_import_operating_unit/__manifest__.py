38627746c4db3d7dad13dcd83c295b491c81fa49
Andhitia Rama <andhitia.r@gmail.com>
2026-08-04 21:24:47 +0700

    [ADD] ssi_data_import_operating_unit
    Menambahkan modul glue ssi_data_import_operating_unit yang memberi
    dokumen data_import kepemilikan Operating Unit:
    
    * Model data_import diwarisi ulang dengan mixin.single_operating_unit
      (ssi_operating_unit_mixin) sehingga memikul field operating_unit_id,
      default dari operating unit pengguna.
    * Grup data ownership Operating Unit baru pada kategori data ownership
      modul basis, diimplikasikan oleh grup Company yang sudah ada.
    * Record rule baru yang membatasi akses data_import ke operating unit
      milik pengguna untuk grup Operating Unit tersebut.
    * Field operating_unit_id disisipkan setelah company_id pada search,
      tree, dan form data_import beserta filter group-by Operating Unit.
    * Unit test skenario YAML (assignment eksplisit + default dari user)
      dan test Python murni (P5/L-22) untuk penolakan referensi operating
      unit yang tidak valid.
    
    Closes #14

---
+++ ssi_data_import_operating_unit/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Data Import - Operating Unit Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "contributors": [
+        "Andhitia Rama <andhitia.r@gmail.com>",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_data_import",
+        "ssi_operating_unit_mixin",
+    ],
+    "data": [
+        "security/res_group/data_import.xml",
+        "security/ir_rule/data_import.xml",
+        "views/data_import.xml",
+    ],
+    "demo": [],
+}
---
+++ ssi_data_import_operating_unit/models/data_import.py
@@ -0,0 +1,15 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class DataImport(models.Model):
+    """Data import document scoped to a single operating unit."""
+
+    _name = "data_import"
+    _inherit = [
+        "data_import",
+        "mixin.single_operating_unit",
+    ]
